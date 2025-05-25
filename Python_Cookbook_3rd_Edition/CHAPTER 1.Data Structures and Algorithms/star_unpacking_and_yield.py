ae603a9d14bbefe4ee2f695ab77b779416579957
李红睿 <604109142@qq.com>
2025-05-25 21:53:17 +0800

    init and update star unpacking in Python Cookbook.

---
+++ Python_Cookbook_3rd_Edition/CHAPTER 1.Data Structures and Algorithms/star_unpacking_and_yield.py
@@ -0,0 +1,11 @@
+'''The usage of star unpacking and yield grammer'''
+# Python “star expressions” can be used to address this problem.
+# So, what would happen if there are more than two arguments?
+record = ('Dave', 'dave@example.com', '773-555-1212', '847-555-1212')
+name, email, *phone_numbers = record
+print(phone_numbers)  # ['773-555-1212', '847-555-1212']
+
+# name, email, *list_1, *list2 = record
+#    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# SyntaxError: multiple starred expressions in assignment
+
