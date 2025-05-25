7fc785eee8de3daa619187e3efb0564d53f5daac
李红睿 <604109142@qq.com>
2025-05-25 22:01:25 +0800

    Add module doc.

--- Python_Cookbook_3rd_Edition/CHAPTER 1.Data Structures and Algorithms/star_unpacking_and_yield.py
+++ Python_Cookbook_3rd_Edition/CHAPTER 1.Data Structures and Algorithms/star_unpacking_and_yield.py
@@ -1,4 +1,10 @@
-'''The usage of star unpacking and yield grammer'''
+'''
+The usage of star unpacking and yield grammar in Python
+
+For more context and summary of the book, please refer to:
+https://gou7ma7.github.io/2025/05/25/book/Python_Cookbook_3rd_Edition/#star-unpacking
+
+'''
 # Python “star expressions” can be used to address this problem.
 # So, what would happen if there are more than two arguments?
 record = ('Dave', 'dave@example.com', '773-555-1212', '847-555-1212')
